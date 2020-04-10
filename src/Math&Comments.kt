7f8c494ea166f82ba91fb80a09f9e5566cc8dd31
coderkhem <khemagarwal1@gmail.com>
2020-04-10 19:11:25 +0530

    Initial commit

---
+++ src/Math&Comments.kt
@@ -0,0 +1,26 @@
+fun main() {
+    //This is single line comment in Kotlin
+
+    /*
+    This
+    is
+    multiline
+    comment
+    in
+    kotlin
+     */
+
+    //circumference of a circle
+
+    var radius=2
+    var pi=3.14
+
+    var c: Int= 2 * pi.toInt() * radius
+
+    println(c)
+
+    var age= 21
+    age =+ 40
+    
+
+}
---
+++ src/ifstatement.kt
@@ -0,0 +1,21 @@
+fun main() {
+    var isDogAlive: Boolean = true
+
+if(isDogAlive)
+{
+
+}
+    else
+{
+
+}
+    //
+    var name="khem"
+
+    if(name.equals("Nick"))
+    {
+        println("Nick is awesome!!")
+    }
+    else
+        println("$name is awesome!!")
+}
