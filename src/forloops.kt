b9f62e1f8526acfe2751ef0fb7bf573a86e168be
coderkhem <khemagarwal1@gmail.com>
2020-04-10 19:25:05 +0530

    Initial commit

---
+++ src/forloops.kt
@@ -0,0 +1,23 @@
+fun main() {
+    //
+    for(x in 1..10){
+        println(x)
+    }
+    //
+    var movies= listOf("never back down","rocky","hera pheri")
+
+    for(x in movies)
+    {
+        println(x)
+    }
+
+    //
+    for(x in 1..200)
+    {
+        if(x%2 != 0)
+        {
+            println(x)
+        }
+    }
+
+}
---
+++ src/lists&arrays.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var movies= listOf("never back down","rocky","hera pheri")
+
+    println(movies[0])
+
+    var arrayExample= arrayOf(1,2,4)
+
+    var mutableExample= mutableListOf(1,2,3)
+    mutableExample.add(1,5);
+    println(mutableExample)
+}
