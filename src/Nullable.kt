4177f8cc25c86446ec72bf968a1ea278dc23d581
coderkhem <khemagarwal1@gmail.com>
2020-04-10 22:55:04 +0530

    Initial commit

---
+++ src/Nullable.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    var age: Int? =28
+    var name: String?="khem"
+
+    if (age != null)
+    {
+        age!!
+    }
+
+    var dogs= mapOf("Fido" to 9)
+    var dogAge=dogs["abc"]
+
+
+ 
+}
