cdb388dae4b308e16ecfe4c38250a3bed248fb83
coderkhem <khemagarwal1@gmail.com>
2020-04-10 19:35:47 +0530

    Initial commit

---
+++ src/Maps.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var dogs= mapOf("A" to 5,"B" to 6)
+    println(dogs["B"])
+
+    var cats= mutableMapOf("C" to 3,"D" to 4)
+    cats["E"]=5
+    println(cats)
+}
---
+++ src/functions.kt
@@ -0,0 +1,13 @@
+fun main() {
+    fun hello(): String
+    {
+        return "Hello"
+    }
+
+    fun dog(name: String,age: Int)
+    {
+        println("Hello my name is $name  and my age is $age" )
+    }
+println(dog("A",5))
+
+}
